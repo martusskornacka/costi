a66c57e01f8163912a6382d4ffa173f04e33ad5e
Marta Kornacka <40639586+martusskornacka@users.noreply.github.com>
2019-07-14 21:30:24 +0200

    Update AddTask.js

--- AddTask.js
+++ AddTask.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import './AddTask.css'
 
 class AddTask extends Component {
-    state = {}
+    state = {
+        text: '',
+        checked: false,
+        date: "2019-09-25",
+    }
     render() {
         return (
             <div className="form">
-                <div>
-                    dodaj task
-            </div>
+                <input type="text" placeholder="dodaj zadanie" value={this.state.text} />
+                <input type="checkbox" checked={this.state.checked} id={important} />
+                <label htmlFor="important">Priorytet</label>
+                <br />
+                <label htmlFor="date">Do kiedy zrobić</label>
+                <input type="date" value={this.state.date} min="2019-01-01" max="2020-12-31" />
+                <button>Dodaj</button>
+                <br />
             </div>
 
         );
