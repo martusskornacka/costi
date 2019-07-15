99a6b8b903158dd6e8b3de459b44b2e4a7a27f05
Marta Kornacka <40639586+martusskornacka@users.noreply.github.com>
2019-07-15 21:46:44 +0200

    Update TaskList.js

--- TaskList.js
+++ TaskList.js
@@ -5,6 +5,36 @@ const TaskList = () => {
 
     const active = props.tasks.filter(task => task.active);
     const done = props.tasks.filter(task => !task.active);
+
+    // done.sort((a, b) => b.finishDate - a.finishDate)
+    //0
+    //<0
+    //>0
+
+    if (done.length >= 2) {
+        done.sort((a, b) => {
+            if (a.finishDate < b.finishDate) {
+                return 1
+            }
+            if (a.finishDate > b.finishDate) {
+                return -1
+            }
+            return 0
+        })
+    }
+
+    if (active.length >= 2) {    //funkcja porównujaca
+        active.sort((a, b) => {
+            a = a.text.toLowerCase()
+            b = b.text.toLowerCase()
+
+            if (a < b) return -1
+            if (a > b) return 1;
+            return 0
+        })
+
+    }
+
     const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
     const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
 
